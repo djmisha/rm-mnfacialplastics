09267bdfc23af229e22ff0c4331ae908bbcd6cdc
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2019-10-01 11:09:09 -0700

    splash pages

--- inc/partials/procedures-repeater.php
+++ inc/partials/procedures-repeater.php
@@ -1,10 +1,9 @@
-
 <section class="flexible-procedures-repeater">
 	<?php if(have_rows('the-procedures')): ?>
 		<ul>
 			<?php while(have_rows('the-procedures')): the_row(); ?>
 				<li id="<?php the_sub_field('id'); ?>">
-					<?php the_sub_field('image'); ?>
+					<img class="b-lazy" data-src="<?php the_sub_field('image'); ?>" alt="icon">
 					<?php the_sub_field('content'); ?>
 					<a href="<?php the_sub_field('link'); ?>" class="button" rel="nofollow"> Learn More</a>
 				</li>
--- inc/partials/split-section.php
+++ inc/partials/split-section.php
@@ -9,20 +9,20 @@
 
 
 
-<section>
+<div class="page-feature-wrap">
   <div class="page-feature flexible-split-section" id="<?php echo $id; ?>">
     <div class="feature-content">
       <div class="box-with-border">
         <div class="inside-box">
           <h2><?php echo $headline; ?></h2> 
-            <?php echo $content; ?>
+          <?php echo $content; ?>
         </div>
       </div>
     </div>
-    <div class="feature-image b-lazy" data-src="<?php echo $bg; ?>">
-    </div>
+    <div class="feature-image b-lazy" data-src="<?php echo $bg; ?>"></div>
+    <div class="bg-underlay"></div>
   </div>
-</section>
+</div>
 
 
   
