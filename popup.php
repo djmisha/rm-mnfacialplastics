33902e0f0fdbe825e7977c5d81aad2194821e5c1
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2019-10-09 09:25:53 -0700

    qc

--- popup.php
+++ popup.php
@@ -8,50 +8,13 @@
 	
 	<style>
 		
-		#notice-body {
-			width: 100%;
-			height: 100%;
-			display:inline-block;
-		}
-		
-		#notice-body-everything {
-			max-width: 500px;
-			height: auto;
-			padding:20px;
-			display:inline-block;
-		}
-		
-		#notice-body-everything h2 {
-			text-transform: uppercase;
-			font-size: 40px;
-			margin:20px 0 30px 0;
-			color: #1c90c2;
-		}
-		
-		#notice-body p {
-			margin:0 0 30px 0;
-			color: #0b1d53;
-			font-weight: bold;
-			font-size: 17px;
-		}
-		
-		#notice-body-everything #buttons {
-			display: block;
-			text-align: center;
-			box-shadow: none;
-		}
-
-		#notice-body-everything #buttons a{
-			/*box-shadow:1px 1px 1px 0px #333;*/
-			font-weight: bold;
-			font-size: 20px;
-		}
+	
 	</style>
 
 
-<div id="notice-body">
+<div id="gallerypop-notice-body">
 	<div id="notice-body-everything">
-		<!-- <h2>Notice</h2> -->
+		<h2>Notice</h2>
 		<p>Dr. Prischmann and her team would like to express gratitude to the generous patients who have agreed to allow their photos to be part of this gallery. This gallery is intended to be educational and informative. Due to the sensitive nature of these photographs, you must be at least 18 years of age to view the gallery. If you are at least 18 years of age and wish to continue, please click the 'OK' button now.</p>
 		<div id="buttons"><a href="javascript:parent.jQuery.fancybox.close();" class="popupbtn button">OK</a></div>
 	</div>
